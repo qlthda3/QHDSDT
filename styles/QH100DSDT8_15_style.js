01a9a8c478e8bae960fe83f972bbe52ad8ba2baa
qlthda3 <qlthda3.mrb@gmail.com>
2026-02-13 10:14:44 +0700

    Add files via upload

---
+++ styles/QH100DSDT8_15_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_QH100DSDT8_15 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,0,255,1.0)', lineDash: [13.072,6.536,3.268,6.536], lineCap: 'butt', lineJoin: 'miter', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
