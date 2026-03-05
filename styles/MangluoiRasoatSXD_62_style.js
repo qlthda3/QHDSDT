43a1f9d00dde4f8f84459b09d4ba8bc10dea5dd0
qlthda3 <qlthda3.mrb@gmail.com>
2026-03-05 15:57:45 +0700

    Add files via upload

---
+++ styles/MangluoiRasoatSXD_62_style.js
@@ -0,0 +1,153 @@
+var size = 0;
+var placement = 'point';
+function categories_MangluoiRasoatSXD_62(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Tuyến 1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(64,219,123,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 10':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(201,21,99,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 11':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(115,196,212,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 12':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(161,221,114,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(96,100,223,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 2A':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(135,91,212,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 3':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(182,212,32,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 4':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(91,143,212,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 5':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(237,123,58,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 6':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(229,138,141,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 7':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(174,40,215,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 8':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(138,229,209,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến 9':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(219,187,81,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tuyến mới':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(215,17,182,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(39,217,36,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_MangluoiRasoatSXD_62 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Layer");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_MangluoiRasoatSXD_62(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
